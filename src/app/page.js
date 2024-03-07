e0e1c0aaca454cff231d99de4c3ffd75527445cc
Viacheslav Plevako <cheryec@ukr.net>
2024-03-07 21:13:25 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
+"use client";
+
 import { Header } from "@/Component/Header/Header";
 import { Hero } from "@/Component/Hero/Hero";
 import styles from "./page.module.css";
 import { OurUsers } from "@/Component/OurUsers/OurUsers";
 import { PostForm } from "@/Component/PostForm/PostForm";
 import { getPosition } from "@/utils/user";
-import { use } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const positions = use(getPosition());
+  const [positions, setPositions] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const get = await getPosition();
+      setPositions(get);
+    })();
+  }, []);
+
+  // const positions = use();
 
   return (
     <main>
