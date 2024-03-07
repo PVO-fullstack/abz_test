5e31f3da7bfb2a84c5f3235e8c60bd38af40e264
Viacheslav Plevako <cheryec@ukr.net>
2024-03-07 20:15:59 +0200

    Update Hero.jsx

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -12,9 +12,9 @@ export const Hero = () => {
         <p className={styles.description}>
           What defines a good front-end developer is one that has skilled
           knowledge of HTML, CSS, JS with a vast understanding of User design
-          thinking as they'll be building web interfaces with accessibility in
-          mind. They should also be excited to learn, as the world of Front-End
-          Development keeps evolving.
+          thinking as they&apos;ll be building web interfaces with accessibility
+          in mind. They should also be excited to learn, as the world of
+          Front-End Development keeps evolving.
         </p>
         <a className={styles.btn} href="#post">
           <p className={styles.text}>Sing up</p>
